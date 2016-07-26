bb9105cfb0b18280839881008d3409f88c96be09
Bakyaraj Renu <AL1348@accionlabs.com>
2016-07-26 16:05:24 +0530

    Adding josn file

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-### angular js setup
-`
-  npm install
-  bower 
-  npm run server
-`
+Quick Start angularsetup With Grunt AnD Bower
+Step1 : Install Node.js
+
+ git clone https://github.com/bakyaraj/angularsetup.git
+ cd angularsetup
+ npm -g install grunt-cli karma bower
+ npm install
+ bower install
+Start Your Application "User live-server"
--- public/app/controller/Mainctrl.js
+++ public/app/controller/Mainctrl.js
@@ -1,18 +1,51 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular
-	  .module('myapp')
-	  .controller('MainController', MainController);
+    angular
+        .module('angular-fancytree')
+        .controller('ExampleController', ExampleController);
 
-	MainController.$inject = ['$scope','fancytreeFactory'];
+    ExampleController.$inject = ['$scope','$http','$injector', 'fancytreeFactory'];
 
-	function MainController($scope,fancytreeFactory) {
-		//content
-		$scope.name="BAKI"
-		console.log(fancytreeFactory);
-		 // Passing fancytree options
-        // second arg is fancytree's element id
-        
-	}
+    function ExampleController($scope,$http,$injector, fancytreeFactory) {
+        $http.get("test.json").then(function(response) {
+            $scope.fancyData = response.data.data;
+        });
+
+        //Once i use this this response i am getting some error cause fancytreeFactory tree configuration differnt form json formate
+        fancytreeFactory.setData([
+            {title: "Node 1", key: "1"},
+            {title: "Folder 2", key: "2", folder: true, children: [
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.2", key: "4"}
+            ]},
+                        {title: "Folder 2", key: "2", folder: true, children: [
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.1", key: "3", myOwnAttr: "abc"},
+                {title: "Node 2.2", key: "4"}
+            ]}
+
+
+        ], 'exampleFancytree');
+
+        fancytreeFactory.setMethods({
+            "select": function (event, data) {
+                var ExampleService = $injector.get('ExampleService');
+                var node = data.node;
+
+                if (node.isSelected()) {
+                    ExampleService.doSomething(node.title);
+                } else {
+                    ExampleService.doSomethingElse(node.title);
+                }
+            }
+        }, 'exampleFancytree');
+    }
+
 })();
+
